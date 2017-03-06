52eb715d497ff9c0da469771284090b18dfc834b
JonCosta <jonjcosta@gmail.com>
2017-03-06 09:44:41 -0300

    capitulo03-aula03 [v2]

---
+++ src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { NgModule, ErrorHandler } from '@angular/core';
+import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
+import { MyApp } from './app.component';
+import { HomePage } from '../pages/home/home';
+import { MenuTestPage } from '../pages/menu-test/menu-test';
+
+
+@NgModule({
+  declarations: [
+    MyApp,
+    HomePage,
+    MenuTestPage
+  ],
+  imports: [
+    IonicModule.forRoot(MyApp)
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+    HomePage,
+    MenuTestPage
+  ],
+  providers: [{provide: ErrorHandler, useClass: IonicErrorHandler}]
+})
+export class AppModule {}
